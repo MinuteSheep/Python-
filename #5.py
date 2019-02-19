352497772e7d69f5fa2c45d81fad6817026ca697
MinuteSheep <546331759@qq.com>
2019-02-19 15:05:39 +0800

    内置函数range的高级用法

---
+++ #5.py
@@ -0,0 +1,15 @@
+#!/Users/minutesheep/.pyenv/shims/python
+# -*- coding: utf-8 -*-
+
+
+test = [1, 7, 3, 5, 'hello', 234, 2]
+
+for i in range(-1, -len(test)-1, -1):
+    print(test[i])
+
+
+'''
+range(-1, -100, -1)
+使用这种方法可以实现从-1到-99的逆向遍历
+学到了吗，弟弟
+'''
